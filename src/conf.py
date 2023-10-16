16eb9d665bd5024f0c668285495bd1ab743106b5
pdmurray <peynmurray@gmail.com>
2023-10-15 21:07:51 -0700

    Add git history to pages

--- src/conf.py
+++ src/conf.py
@@ -1,3 +1,9 @@
+import pathlib
+from collections import defaultdict
+
+import git
+import pandas as pd
+
 # Configuration file for the Sphinx documentation builder.
 #
 # Options for this theme, pydata:
@@ -52,7 +58,7 @@ html_theme = "pydata_sphinx_theme"
 html_theme_options = {
     "footer_start": ["copyright"],
     "footer_end": ["sphinx-version"],
-    "secondary_sidebar_items": ["page-toc", "edit-this-page"],
+    "secondary_sidebar_items": ["page-toc", "edit-this-page", "git-history"],
     "logo": {"text": "OAWiki"},
     "use_edit_page_button": True,
 }
@@ -68,6 +74,48 @@ html_context = {
 html_static_path = ["_static"]
 
 
+def add_context_funcs(app, pagename, templatename, context, doctree):
+    repo_url = "https://github.com/observational-dev/oawiki"
+    repo = git.Repo(pathlib.Path(__file__).parents[1])
+
+    def add_git_history() -> str:
+        """Generate the git history content for each page.
+
+        Returns
+        -------
+        str
+            Git history rendered as HTML.
+        """
+        if "page_source_suffix" in context:
+            filename = (pathlib.Path(__file__).parent / pagename).with_suffix(
+                context["page_source_suffix"]
+            )
+            commit_log = defaultdict(list)
+            for commit in repo.iter_commits(paths=filename):
+                commit_log["Date"].append(
+                    f'<a href="{repo_url}/commit/{commit.hexsha}">'
+                    f"{str(commit.authored_datetime)}</a>"
+                )
+                commit_log["Author"].append(commit.author.name)
+                commit_log["Message"].append(commit.summary)
+            return (
+                pd.DataFrame(commit_log)
+                .sort_values("Date")
+                .to_html(
+                    index=False,
+                    escape=False,
+                    classes="git-history",
+                    justify="center",
+                )
+            )
+        return ""
+
+    context["add_git_history"] = add_git_history
+
+
 def setup(app):
     # Styles applied to the entire wiki
     app.add_css_file("css/custom.css")
+    app.add_js_file("js/custom.js")
+
+    app.connect("html-page-context", add_context_funcs)
