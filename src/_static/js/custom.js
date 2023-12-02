a03712c13c03b55a5f7f79d932632d43d749b2e4
Peyton Murray <peynmurray@gmail.com>
2023-12-01 23:16:15 -0800

    Add snazzy splash page (#53)

--- src/_static/js/custom.js
+++ src/_static/js/custom.js
@@ -4,22 +4,24 @@ function setupGitHistory() {
   const background = document.getElementById('history-modal-background')
   const foreground = document.getElementById('history-modal-foreground')
 
-  // Click on the background to close the modal
-  background.onclick = () => {
-    checkbox.checked = ''
-  }
-
-  // Prevent clicks on the table from closing the modal
-  foreground.onclick = (event) => {
-    event.stopPropagation()
-  }
-
-  // Listen for escape key to close modal
-  document.addEventListener('keydown', (event) => {
-    if (event.key === 'Escape' && checkbox.checked) {
+  if (checkbox) {
+    // Click on the background to close the modal
+    background.onclick = () => {
       checkbox.checked = ''
     }
-  })
+
+    // Prevent clicks on the table from closing the modal
+    foreground.onclick = (event) => {
+      event.stopPropagation()
+    }
+
+    // Listen for escape key to close modal
+    document.addEventListener('keydown', (event) => {
+      if (event.key === 'Escape' && checkbox.checked) {
+        checkbox.checked = ''
+      }
+    })
+  }
 }
 
 window.addEventListener('DOMContentLoaded', () => {
